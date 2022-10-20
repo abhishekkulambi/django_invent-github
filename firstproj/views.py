f9de7a8704fee0def1f6cd8f1f4ebb70e7bc1cbc
abhishekkulambi <abhikulambi@gmail.com>
2022-10-20 14:47:11 +0530

    First Commit

---
+++ firstproj/views.py
@@ -0,0 +1,42 @@
+""" 
+	To render html web pages
+"""
+import random
+from re import A
+from django.http import HttpResponse
+
+from django.template.loader import render_to_string
+from articles.models import Article
+
+def home_view(request):
+
+	"""
+	Take in a request & return Html as a response 
+	"""
+	name = "Abhi"
+	random_id = random.randint(1,4)
+
+	# from the database?
+	article_obj = Article.objects.get(id=random_id)
+    
+
+	context = {
+		"object": article_obj,
+		"title": article_obj.title,
+		"id": article_obj.id,
+		"content": article_obj.content,
+	}
+	#Django Templates
+
+	#1 way to call templates
+	# tmpl = get_template("home-view.html")
+	# tmpl_string = tmpl.render(context=context)
+	
+	HTML_STRING = render_to_string('home-view.html', context=context)
+
+	# HTML_STRING = """ 
+	# 	<h1>{title} (id:{id}!)</h1>
+	# 	<p>{content}</p>
+	# """.format(**context)
+	
+	return HttpResponse(HTML_STRING)
