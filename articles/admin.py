eb89e8afd184824910b427c925b0bf105984f3bc
abhishekkulambi <abhikulambi@gmail.com>
2022-10-21 12:47:44 +0530

    Register models in admin site

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Article
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    search_fields = ['title']
+
+
+
+
+admin.site.register(Article, ArticleAdmin)
