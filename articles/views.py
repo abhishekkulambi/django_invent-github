25e7b1a7d2412c50c0d0b459e5b952c111e88ac8
abhishekkulambi <abhikulambi@gmail.com>
2022-10-21 14:16:46 +0530

    Created basic search form

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,26 @@ from django.shortcuts import render
 from .models import Article
 # Create your views here.
 
+#search_view
+def article_search_view(request):
+    query_dict = request.GET    #this is a dict
+    #query = query_dict.get('q')  #<input type="text" name='q'/>
+    #print(request.GET)
+    try:
+        query = int(query_dict.get("q")) 
+    except:
+        query = None
+
+    article_obj = None 
+    if query is not None:
+        article_obj = Article.objects.get(id=query)   #basic search type
+
+    context = {
+        "object": article_obj
+    }
+    return render(request, "articles/search.html", context=context)
+
+
 def article_detail_view(request, id=None):
 
     article_obj = None
