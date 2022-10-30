5a352d48809d7cbe662dee2bb1ea32a90cc10a9e
abhishekkulambi <abhikulambi@gmail.com>
2022-10-30 20:29:40 +0530

    Login via django Authform

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 
 
@@ -14,19 +14,37 @@ def register_view(request):
 
 
 # Create your views here.
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        #print(username, password)
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error":"Invalid username or password"}
-            return render(request, 'accounts/login.html', context)
-        login(request, user)
-        return redirect('/')
 
-    return render(request, 'accounts/login.html', context={})
+def login_view(request):
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {
+            "form":form
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+
+# def login_view_basic(request):
+ 
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is None:
+#             context = {"error":"Invalid username or password"}
+#             return render(request, 'accounts/login.html', context)
+#         login(request, user)
+#         return redirect('/')
+
+#     return render(request, 'accounts/login.html', context={})
 
 #logout user 
 def logout_view(request):
